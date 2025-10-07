97df9f5bf5e8b3fb386cdbf9a9685711ad74b9e6
Fedir <klumenko356@gmail.com>
2025-10-07 20:03:07 +0300

    Models for game, developer and genre

--- GameStore/store/models.py
+++ GameStore/store/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Developer(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    short_description = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=10, default="USD")
+    released_at = models.DateField(null=True, blank=True)
+    rating = models.FloatField(default=0)
+    developer = models.ForeignKey(Developer, on_delete=models.PROTECT, related_name="games")
+    genres = models.ManyToManyField(Genre, blank=True, related_name="games")
+    stock = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
